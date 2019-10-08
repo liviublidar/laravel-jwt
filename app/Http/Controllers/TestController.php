86ec938fe08afb8dac6b4dd0f43a79a04d49e6e1
Liviu Blidar <liviu.blidar94@gmail.com>
2019-10-08 22:37:04 +0100

    Created UserTrait for ease of access to tokenParser

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\UserTrait;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+    use UserTrait;
+
     public function open()
     {
         $data = "This data is open and can be accessed without the client being authenticated";
@@ -15,7 +18,14 @@ class TestController extends Controller
 
     public function closed()
     {
-        $data = "Only authorized users can see this";
+        $loggedInUser = $this->getAuthenticatedUser();
+        $data = [
+            "message" => "Only authorized users can see this",
+            "debug" => $loggedInUser
+        ];
+
+
+
         return response()->json(compact('data'),200);
     }
 }
